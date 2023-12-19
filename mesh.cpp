fbfb88c0df2d99371acc971a4d31ae1cf5ffa827
jkolby <jkolby@nevada.unr.edu>
2023-12-19 15:32:31 -0800

    properly renders on james' machine at this point

--- mesh.cpp
+++ mesh.cpp
@@ -152,13 +152,16 @@ bool Mesh::InitBuffers() {
 	glBindVertexArray(vao);
 
 	glGenBuffers(1, &VB);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VB);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices, GL_STATIC_DRAW);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
 
 
 	glGenBuffers(1, &IB);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+
 
 	return true;
 }

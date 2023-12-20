69fecf8a66e45766f6f09365672ba98c06399ae4
Emily <62121473+McCue-Emily@users.noreply.github.com>
2023-12-19 16:14:54 -0800

    Update Texture.h

--- Texture.h
+++ Texture.h
@@ -10,35 +10,39 @@ class Texture
 public:
 	Texture();
 	Texture(const char* fileName);
-	Texture(const char* right,
+	/*Texture(const char* right,
 		const char* left,
 		const char* top,
 		const char* bottom,
 		const char* front,
-		const char* back);
+		const char* back);*/
 	bool loadTexture(const char* texFile);
-	bool loadCubeTexture(const char* right,
+	/*bool loadCubeTexture(const char* right,
 		const char* left,
 		const char* top,
 		const char* bottom,
 		const char* front,
-		const char* back);
+		const char* back);*/
 
 	GLuint getTextureID() { return m_TextureID; }
 
+	//GLuint getCubeTextureID() { return m_cubeTextureID; }
+
 private:
 	GLuint m_TextureID;
-	char m_fileNames[6];
+	/*GLuint m_cubeTextureID;
+
+	const char* m_fileNames[6];*/
 
 	bool initializeTexture();
-	bool initializeCubeTexture();
+	//bool initializeCubeTexture();
 
-	const char* right = "asset\\right.png";
-	const char* left = "asset\\left.png";
-	const char* top = "asset\\top.png";
-	const char* bottom = "asset\\bottom.png";
-	const char* front = "asset\\front.png";
-	const char* back = "asset\\back.png";
+	//const char* right = "asset\\right.png";
+	//const char* left = "asset\\left.png";
+	//const char* top = "asset\\top.png";
+	//const char* bottom = "asset\\bottom.png";
+	//const char* front = "asset\\front.png";
+	//const char* back = "asset\\back.png";
 
 };
 
